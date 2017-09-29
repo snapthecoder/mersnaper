39e77aadf5dda689984921784eb5c48b1f57b5f4
MrMerfen <beda.foehn@hotmail.com>
2017-09-29 15:50:35 +0200

    sdf

--- projects/starter/Debug/Project_Settings/Startup_Code/startup.d
+++
@@ -1,8 +0,0 @@
-Project_Settings/Startup_Code/startup.o: \
- ../Project_Settings/Startup_Code/startup.c \
- C:/INTRO/workspace/starter/Generated_Code/IO_Map.h \
- C:/INTRO/workspace/starter/Static_Code/IO_Map/MK22F12.h
-
-C:/INTRO/workspace/starter/Generated_Code/IO_Map.h:
-
-C:/INTRO/workspace/starter/Static_Code/IO_Map/MK22F12.h:
